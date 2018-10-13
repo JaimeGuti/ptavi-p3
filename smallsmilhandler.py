f98c9c2181491df29786b8e5967bd7e98660d66f
JaimeGuti <jaime.m.guti@gmail.com>
2018-10-13 13:58:50 +0200

    Añadidos los métodos de las etiquetas y sus atributos

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -7,11 +7,53 @@ from xml.sax.handler import ContentHandler
 class SmallSMILHandler(ContentHandler):
 
     def __init__(self):
+
         self.attrs = []
         self.tags = []
 
     def startElement(self, name, attrs):
 
+        if name == 'root-layout':
+
+            rl['width'] = ""
+            rl['height'] = ""
+            rl['background-color'] = ""
+
+            self.tags.append(['root-layout', rl])
+
+        elif name == 'region':
+
+            rg['id'] = ""
+            rg['top'] = ""
+            rg['bottom'] = ""
+            rg['left'] = ""
+            rg['right'] = ""
+
+            self.tags.append(['region', rg])
+
+        elif name == 'img':
+
+            im['src'] = ""
+            im['region'] = ""
+            im['begin'] = ""
+            im['dur'] = ""
+
+            self.tags.append(['img', im])
+
+        elif name == 'audio':
+
+            au['src'] = ""
+            au['begin'] = ""
+            au['dur'] = ""
+
+            self.tags.append(['audio', au])
+
+        elif name == 'textstream':
+
+            ts['src'] = ""
+            ts['region'] = ""
+
+            selfs.tags.append(['textstream', ts])
 
     def get_tags(self):
 
