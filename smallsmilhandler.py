abfcf30a1b994bd0890bcb273513aa5929be8b71
JaimeGuti <jaime.m.guti@gmail.com>
2018-10-13 13:37:52 +0200

    Inicializadas las variables

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -1,20 +1,29 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
 class SmallSMILHandler(ContentHandler):
-    
-    def init(self):
-        self.width = ""
-        self.height = ""
-        self.background-color = ""
-        self.id = ""
-        self.top = ""
-        self.bottom = ""
-        self.left = ""
-        self.right = ""
-        self.src = ""
-        self.region = ""
-        self.begin = ""
-        self.dur = ""
+
+    def __init__(self):
+        self.attrs = []
+        self.tags = []
+
+    def startElement(self, name, attrs):
+
+
+    def get_tags(self):
+
+
+
+
+
+if __name__ == "__main__":
+    """
+    Programa principal
+    """
+    parser = make_parser()
+    cHandler = SmallSMILHandler()
+    parser.setContentHandler(cHandler)
+    parser.parse(open('karaoke.smil'))
